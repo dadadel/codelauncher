80400ff4595c4d43ca12d5cacc73529fc3a2c699
dadadel <daouzli@gmail.com>
2014-05-18 23:55:30 +0200

    First commit. show a 3 sections webpage with launch button anbd choice C/Python. Run (compile/run) the code in source code section

---
+++ runcode/runcode.py
@@ -0,0 +1,60 @@
+import subprocess
+import sys
+
+
+class RunCCode(object):
+    
+    def __init__(self, code=None):
+        self.code = code
+        self.compiler = "gcc"
+    
+    def _compile_c_code(self, filename, prog="./running/a.out"):
+        cmd = [self.compiler, filename, "-Wall", "-o", prog]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = p.wait()
+        a, b = p.communicate()
+        self.stdout, self.stderr = a.decode("utf-8"), b.decode("utf-8")
+        return result
+
+    def _run_c_prog(self, cmd="./running/a.out"):
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = p.wait()
+        a, b = p.communicate()
+        self.stdout, self.stderr = a.decode("utf-8"), b.decode("utf-8")
+        return result
+    
+    def run_c_code(self, code=None):
+        filename = "./running/test.c"
+        if not code:
+            code = self.code
+        result_run = "No run done"
+        with open(filename, "w") as f:
+            f.write(code)
+        res = self._compile_c_code(filename)
+        result_compilation = self.stdout + self.stderr
+        if res == 0:
+            self._run_c_prog()
+            result_run = self.stdout + self.stderr
+        return result_compilation, result_run
+
+class RunPyCode(object):
+    
+    def __init__(self, code=None):
+        self.code = code
+    
+    def _run_py_prog(self, cmd="a.py"):
+        cmd = [sys.executable, cmd]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        result = p.wait()
+        a, b = p.communicate()
+        self.stdout, self.stderr = a.decode("utf-8"), b.decode("utf-8")
+        return result
+    
+    def run_py_code(self, code=None):
+        filename = "./running/a.py"
+        if not code:
+            code = self.code
+        with open(filename, "w") as f:
+            f.write(code)
+        self._run_py_prog(filename)
+        return self.stderr, self.stdout
---
+++ webdev.py
@@ -0,0 +1,61 @@
+from flask import Flask, render_template, request
+from runcode import runcode
+app = Flask(__name__)
+
+default_c_code = """#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("C: Youhou!!!\\n");
+    return 0;
+}    
+"""
+
+default_py_code = """import sys
+import os
+
+if __name__ == "__main__":
+    print "PY: Yaouh!!"
+"""
+
+default_rows = "15"
+default_cols = "60"
+
+@app.route("/")
+@app.route("/runc", methods=['POST', 'GET'])
+def runc():
+    if request.method == 'POST':
+        code = request.form['code']
+        run = runcode.RunCCode(code)
+        rescompil, resrun = run.run_c_code()
+    else:
+        code = default_c_code
+        resrun = 'No result!'
+        rescompil = ''
+    return render_template("main.html",
+                           code=code,
+                           target="runc",
+                           resrun=resrun,
+                           rescomp=rescompil,
+                           rows=default_rows, cols=default_cols)
+
+@app.route("/py")
+@app.route("/runpy", methods=['POST', 'GET'])
+def runpy():
+    if request.method == 'POST':
+        code = request.form['code']
+        run = runcode.RunPyCode(code)
+        rescompil, resrun = run.run_py_code()
+    else:
+        code = default_py_code
+        resrun = 'No result!'
+        rescompil = "No compilation for Python"
+    return render_template("main.html",
+                           code=code,
+                           target="runpy",
+                           resrun=resrun,
+                           rescomp=rescompil,#"No compilation for Python",
+                           rows=default_rows, cols=default_cols)
+
+if __name__ == "__main__":
+    app.run()
